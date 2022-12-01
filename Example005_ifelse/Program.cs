cb9e1781b54c3590500cd40224e000680a20598f
Lyubov <lyubasher@mail.ru>
2022-12-01 23:13:29 +0200

    If Else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yes, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
